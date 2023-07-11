22e717c5624c44f5ec6695705903fc5799502dc7
Sejin Lee <lsj8706@naver.com>
2023-07-11 17:11:38 +0900

    [Feat] Stack 구현

---
+++ Sources/SwiftDataStructure/Stack/Stack.swift
@@ -0,0 +1,46 @@
+//
+//  Stack.swift
+//  
+//
+//  Created by sejin on 2023/07/11.
+//
+
+import Foundation
+
+public struct Stack<T> {
+    
+    // MARK: - Properties
+    
+    private var info = [T]()
+    
+    /// O(1)
+    public var count: Int {
+        self.info.count
+    }
+    
+    /// O(1)
+    public var isEmpty: Bool {
+        self.info.isEmpty
+    }
+    
+    /// O(1)
+    public var top: T? {
+        self.info.last
+    }
+    
+    // MARK: - initialization
+    
+    public init() {}
+    
+    // MARK: - Methods
+    
+    /// O(1)
+    public mutating func push(_ item: T) {
+        self.info.append(item)
+    }
+    
+    /// O(1)
+    public mutating func pop() -> T? {
+        return info.popLast()
+    }
+}
