2c5960d0761f21c62fdfc2b8e3bb4c8ac6370f0e
Lee SeJin <lsj8706@naver.com>
2023-07-24 17:36:59 +0900

    [Test] SelectionSort 테스트 코드 작성

---
+++ Tests/SwiftDataStructureTests/SortingTests/SortingTests.swift
@@ -0,0 +1,29 @@
+//
+//  SortingTests.swift
+//  
+//
+//  Created by sejin on 2023/07/24.
+//
+
+import Foundation
+
+import XCTest
+@testable import SwiftDataStructure
+
+final class SortingTests: XCTestCase {
+    
+    private let target: [Int] = [3,1,4,5,2,8,9,7,6]
+    private lazy var sortedTarget = self.target.sorted()
+ 
+    func test_straight_sort() {
+        let result = Sorting.selectionSort(elements: target)
+        XCTAssertEqual(result, sortedTarget)
+    }
+    
+    func test_straight_sort_중복_아이템() {
+        let target = [5,3,4,4,1,1,2]
+        let sortedTarget = target.sorted()
+        let result = Sorting.selectionSort(elements: target)
+        XCTAssertEqual(result, sortedTarget)
+    }
+}
